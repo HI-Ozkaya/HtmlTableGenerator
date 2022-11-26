aae0de56082a7edba1d0a9678921085ced226993
Halil Ozkaya <halil94.123@gmail.com>
2022-11-26 17:32:20 +0300

    Read File

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 
+string path = Path.Combine(Environment.CurrentDirectory, "movies.json");
+string jsonMovies = File.ReadAllText(path);
+
+Console.WriteLine(jsonMovies);
