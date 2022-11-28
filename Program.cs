54cd3d84c2be5ce93c600030948e85e689d2cc88
Halil Ozkaya <79472039+HI-Ozkaya@users.noreply.github.com>
2022-11-28 14:02:26 +0300

    Whitespaces Removed

--- Program.cs
+++ Program.cs
@@ -13,30 +13,30 @@ options.PropertyNameCaseInsensitive = true;
 Movies = JsonSerializer.Deserialize<List<Movie>>(jsonMovies, options);
 
 string htmlTable = "";
-htmlTable += "<table>\n";
 
-htmlTable += "\t<th>\n";
-htmlTable += "\t\t<tr>\n";
-htmlTable += "\t\t\t<td>id</td>\n";
-htmlTable += "\t\t\t<td>title</td>\n";
-htmlTable += "\t\t\t<td>rating</td>\n";
-htmlTable += "\t\t\t<td>genre</td>\n";
-htmlTable += "\t\t\t<td>duration</td>\n";
-htmlTable += "\t\t</tr>\n";
-htmlTable += "\t</th>\n";
+htmlTable += "<table>";
+htmlTable += "<th>";
+htmlTable += "<tr>";
+htmlTable += "<td>id</td>";
+htmlTable += "<td>title</td>";
+htmlTable += "<td>rating</td>";
+htmlTable += "<td>genre</td>";
+htmlTable += "<td>duration</td>";
+htmlTable += "</tr>";
+htmlTable += "</th>";
 
-htmlTable += "\t<tbody>\n";
+htmlTable += "<tbody>";
 foreach (var item in Movies)
 {
-    htmlTable += "\t\t<tr>\n";
-    htmlTable += "\t\t\t<td>" + item.Id + "</td>\n";
-    htmlTable += "\t\t\t<td>" + item.Title + "</td>\n";
-    htmlTable += "\t\t\t<td>" + item.Rating + "</td>\n";
-    htmlTable += "\t\t\t<td>" + item.Genre + "</td>\n";
-    htmlTable += "\t\t\t<td>" + item.Duration + "</td>\n";
-    htmlTable += "\t\t</tr>\n";
+    htmlTable += "<tr>";
+    htmlTable += "<td>" + item.Id + "</td>";
+    htmlTable += "<td>" + item.Title + "</td>";
+    htmlTable += "<td>" + item.Rating + "</td>";
+    htmlTable += "<td>" + item.Genre + "</td>";
+    htmlTable += "<td>" + item.Duration + "</td>";
+    htmlTable += "</tr>";
 }
-htmlTable += "\t</tbody>\n";
+htmlTable += "</tbody>";
 htmlTable += "</table>";
 
 path = Path.Combine(rootPath, "output.html");
